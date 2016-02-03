a969412360890fce4500c1efcbda3367030811ea
Amartya <amartya@cse.iitk.ac.in>
2016-02-03 22:46:54 +0530

    Added conversion to In-Memory

---
+++ asgn2/ThreeOp.py
@@ -0,0 +1,7 @@
+class ThreeOp(object):
+    InstrType = None
+    SymtabEntry1 = None
+    SymtabEntry2 = None
+    SymtabEntry3 = None
+    Target = None
+    Operator = None
--- asgn2/input.py
+++ asgn2/input.py
@@ -2,7 +2,8 @@
 import ply.lex as lex
 import numpy as np
 import sys
-
+from ThreeOp import ThreeOp
+from pprint import pprint
 fname=sys.argv[1]
 #Read the File
 #print fname
@@ -17,9 +18,15 @@ input_lines=data.split("\n")
 
 for i in range(0,len(input_lines)):
 	tmp_list=input_lines[i].split(",")
-	list_of_3ac.append(tmp_list)
+        OpCode = ThreeOp()
+        OpCode.InstrType = "Math"
+        OpCode.SymtabEntry1 = tmp_list[0]
+        OpCode.SymtabEntry2 = tmp_list[2]
+        OpCode.SymtabEntry3 = tmp_list[4]
+        OpCode.Operator = tmp_list[3]
+	list_of_3ac.append(OpCode)
 
-print list_of_3ac
+pprint ([vars(x) for x in list_of_3ac])
 
 
 
