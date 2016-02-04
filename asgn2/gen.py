3093cbcf11bde43b75e43dc4ea57f65f7c7c60d1
Kriti <kritij@iitk.ac.in>
2016-02-04 18:19:23 +0530

    gen code added

---
+++ asgn2/gen.py
@@ -0,0 +1,26 @@
+from helperScripts import *
+from DataStruct import *
+
+def symbol(op):
+	if op=='+':
+		return "ADDL"
+	elif op=='-':
+		return "SUBL"
+	elif op=='*':
+		return "IMUL"
+	elif op=='/':
+		return "IDIV"
+
+def gen(ops,zdash,L):
+	opr=symbol(ops.Operator)
+	if check_variable(zdash):
+		print opr+" $"+str(zdash)+",%"+L
+	elif zdash==ops.SymtabEntry3:
+		print opr+" "+zdash+",%"+L
+	else:
+		print opr+" %"+zdash+",%"+L
+
+op=ThreeOp()
+op.SymtabEntry3=3
+op.Operator='*'
+gen(op,3,'EAX')
