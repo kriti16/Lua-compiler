73d302384bb611d4515ab8980f32294c07eddce2
Kriti <kritij@iitk.ac.in>
2016-04-04 23:38:32 +0530

    final submission

---
+++ irspitter/asgn4/test/fun.lua
@@ -0,0 +1,30 @@
+h = 3333
+function f(jj)
+   j = 2
+   if j > 1 then
+      h = 2
+   elseif j >3 then
+      function g()
+         t = 2
+         if t == 2 then
+            k = 2
+            return k
+         else
+            k = 4
+         end
+         return g(k)
+      end
+      h = g()
+   else
+      h = 3
+   end
+   return h
+end
+dd = f(h)
+print(dd)
+two(1,2)
+
+function two(a,b)
+   c = a + b
+   print(c)
+end
---
+++ irspitter/asgn4/test/ifelse.lua
@@ -0,0 +1,12 @@
+x = 2
+z = 5
+if x>1 and not(z ==2 or x >5) then
+   y = 2
+   if x == 2 then
+      x = x + 1
+   end
+elseif x==1 or z<2 then
+   y =  4
+else
+   y = 5
+end
