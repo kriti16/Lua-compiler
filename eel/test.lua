4391b12247ed0ea3e23637da36485921c607e1e0
Amartya <amartya@cse.iitk.ac.in>
2016-04-09 19:38:31 +0530

    Some more changes

--- eel/test.lua
+++ eel/test.lua
@@ -1,26 +1,7 @@
-y = 1
-pp = 8
-z = 0
-if y == 1 then
-   z = 500
-   repeat
-      z = z-1
-      if z% 2==0 then
-         pp = pp - z
-      else
-         function f()
-            if pp %2 == 0 then
-               return 3
-            else
-               return 4
-            end
-         end
-         pp = pp+f()
-         
-      end
-      until z<0
-   
-   y = 3
+z = 2
+while z<100 do
+   if z% 3 == 0 or z%7==0 then
+      print(z)
+   end
+   z = z+1
 end
-p = y + z + pp
-print(pp)
