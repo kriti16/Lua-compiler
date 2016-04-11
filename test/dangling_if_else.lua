c3c066512c9ee4266f9e4483e442a4a73805e5a4
Kriti <kritij@iitk.ac.in>
2016-04-11 17:18:05 +0530

    test cases for asgn3 and asgn4

---
+++ test/dangling_if_else.lua
@@ -0,0 +1,10 @@
+i = 0
+a = 0
+if i <= 3 then 
+    a = a + 1
+end
+if i>=2 then
+        a = a - 1
+    else 
+        a = 1
+end
---
+++ test/incorrect.lua
@@ -0,0 +1,8 @@
+i = 6
+while(;i<= 8 and i 6 and i~= 7) do
+    if i>=0 ; do
+        print("yes\n");
+    else 
+        print("no\n");
+  	end
+end
