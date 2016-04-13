cc2ef49fde0376eec0bbc937288a9c17ac8ca919
Amartya <amartya@cse.iitk.ac.in>
2016-04-13 23:07:11 +0530

    Number indexin

--- eel/out.s
+++ eel/out.s
@@ -1,6 +1,4 @@
 .section .data
-t4:
-  .long 0
 t2:
   .long 0
 t3:
@@ -8,6 +6,12 @@ t3:
 t0:
   .long 0
 t1:
+  .long 0
+3:
+  .long 0
+2:
+  .long 0
+y:
   .long 0
 _empty_:
   .long 0
@@ -23,76 +27,35 @@ strfmt:  .asciz "%s\n"
 main:
 
 LEE1:
-	MOVL $2,%EAX
+	CALL CreateDict
 	MOVL %EAX,t0
-	PUSHL t0
-	PUSHL $fmtstr
-	CALL printf
+	MOVL t0,%EAX
+	PUSHL $2
+	MOVL %EAX,t0
+	MOVL %EAX,t1
+	CALL itoa
+	MOVL %EAX,t2
+	PUSHL $3
+	PUSHL t2
+	PUSHL t1
+	CALL insertDict
+	MOVL %EAX,t1
+	ADDL $12, %ESP
+	PUSHL $2
+	CALL itoa
+	MOVL %EAX,t3
+	PUSHL t3
+	PUSHL t1
+	CALL getDict
+	MOVL %EAX,y
 	ADDL $8, %ESP
-	CALL g1
-	MOVL %EAX,t3
-	ADDL $0, %ESP
-	MOVL t3,%EAX
-	MOVL %EAX,t3
-	MOVL %EAX,t4
-	PUSHL t4
+	PUSHL y
 	PUSHL $fmtstr
 	CALL printf
 	ADDL $8, %ESP
 	MOVL $0,%EAX
-	MOVL %EAX,_empty_
 
 	MOVL $1,%EAX
 	MOVL $0,%EBX
 int $0x80
 
-f1:
-	MOVL $222,%EAX
-	MOVL $3333,%EBX
-	MOVL %EAX,t0
-	MOVL %EBX,t1
-	PUSHL t1
-	PUSHL $fmtstr
-	CALL printf
-	ADDL $8, %ESP
-	PUSHL t0
-	PUSHL $fmtstr
-	CALL printf
-	ADDL $8, %ESP
-	CALL z
-	MOVL %EAX,t2
-	ADDL $0, %ESP
-	MOVL t2,%EAX
-	MOVL %EAX,t2
-	MOVL %EAX,t0
-	PUSHL t0
-	PUSHL $fmtstr
-	CALL printf
-	ADDL $8, %ESP
-	MOVL $322,%EAX
-	MOVL %EAX,t0
-	PUSHL t0
-	PUSHL $fmtstr
-	CALL printf
-	ADDL $8, %ESP
-	MOVL t0,%EAX
-	RET
-	MOVL $0,%EAX
-	MOVL %EAX,_empty_
-z:
-	MOVL $3,%EAX
-	MOVL %EAX,t0
-	PUSHL t0
-	PUSHL $fmtstr
-	CALL printf
-	ADDL $8, %ESP
-	MOVL $6,%EAX
-	RET
-	MOVL $0,%EAX
-	MOVL %EAX,_empty_
-g1:
-	MOVL $333,%EAX
-	MOVL %EAX,t0
-	MOVL t0,%EAX
-	RET
-	MOVL $0,%EAX
