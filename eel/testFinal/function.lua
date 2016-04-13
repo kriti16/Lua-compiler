7d0cc1ca862c70c79d106baa0350c473c7ac7b73
Kriti <kritij@iitk.ac.in>
2016-04-13 21:24:51 +0530

    function test

---
+++ eel/testFinal/function.lua
@@ -0,0 +1,31 @@
+y = 2
+
+-- function f1()
+--    y = 222
+--    do
+--       local y = 3333
+--       print(y)
+--    end
+--    print(y)
+--    y = z()
+--    print(y)
+--    function z()
+--       y = 3
+--       print(y)
+--       return 6
+--    end
+
+--    y = 322
+--    print(y)
+--    return y
+-- end
+
+print(y)
+
+function g1()
+   y = 333
+   return y
+end
+
+dd = g1()
+print(dd)
